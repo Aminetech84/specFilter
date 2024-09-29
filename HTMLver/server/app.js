95299d91a4e49dd74ea9e5d25b4d01ab4005be8b
Amine Ouldaouali <aminetech84@hotmail.com>
2024-09-29 07:58:06 +0100

    Add ENV file and fix typos

--- HTMLver/server/app.js
+++ HTMLver/server/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 //const router= express.Router();
 const mongoose = require("mongoose");
 const cors = require("cors");
+const dotenv = require("dotenv");
 //const specialtyController = require('./controllers/specialtyController');
 
 //const Specialty = require('./models/Specialty');
@@ -11,20 +12,17 @@ const cors = require("cors");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+dotenv.config();
 
 // Middleware
 app.use(cors());
 app.use(express.json());
 
-// MongoDB Atlas connection string
-const mongoURI =
-  "mongodb+srv://aminetech84:Azer1st628@cluster0.tptbq.mongodb.net/test?retryWrites=true&w=majority";
-
 // http://localhost:5000/api/specialties
 
 // Connect to MongoDB Atlas
 mongoose
-  .connect(mongoURI)
+  .connect(process.env.MONGODB_URI)
   .then(() => console.log("MongoDB Atlas connected"))
   .catch((err) => console.error("MongoDB connection error:", err));
 
